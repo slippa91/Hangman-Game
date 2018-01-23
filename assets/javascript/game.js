2eb21b1f317fe89093c91746be61a59d00455e36
Susan Lippa <sfl@Susans-MacBook-Pro.local>
2018-01-22 19:37:35 -0600

    letter compare working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,56 +20,63 @@ var livesNumberRender = document.querySelector("#livesNumber");
 
 var startGame = function(event) {
 
+        //Starting Section
+
         var chosenWordArray = wordArray[(Math.floor((Math.random() *10) +1) - 1)];
-            //console.log(chosenWordArray);
+                //console.log(chosenWordArray);
 
         var letterArray = chosenWordArray.split("");
-            //console.log(letterArray);// letterArray[0], letterArray[1], letterArray[2], letterArray[3], letterArray[4]);
+                //console.log(letterArray);// letterArray[0], letterArray[1], letterArray[2], letterArray[3], letterArray[4]);
+                //console.log(typeof letterArray[0]);
 
         var letterOne = document.querySelector("#letter1").innerText = letterArray[0];
-        //console.log(letterOne);
+                //console.log(letterOne);
         var letterTwo = document.querySelector("#letter2").innerText = letterArray[1];
-        //console.log(letterTwo);
+                //console.log(letterTwo);
         var letterThree = document.querySelector("#letter3").innerText = letterArray[2];
-        //console.log(letterThree);
+                //console.log(letterThree);
         var letterFour = document.querySelector("#letter4").innerText = letterArray[3];
-        //console.log(letterFour);
+                //console.log(letterFour);
         var letterFive = document.querySelector("#letter5").innerText = letterArray[4];
-        //console.log(letterFive);  
+                //console.log(letterFive);  
         
         directions.innerText = "Choose a letter using the keyboard.";
         directionsBox.style.backgroundColor = "violet";
         hangmanPicture.src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_0.jpeg"; 
 
 
+        //User Guess
 
+        var chosenLetter = function(event) {
 
-
-                var chosenLetter = function(event) {
-
-                        var userLetter = event.key;
-                        
+                var userLetter = String(event.key);
                         console.log("letter", userLetter);
-                
+                        console.log(typeof userLetter);
+                        console.log("letterArray", letterArray, letterArray[0], letterArray[1], letterArray[2], letterArray[3], letterArray[4]);            
+                        console.log(typeof letterArray[0]);
 
-                        function compareLetter() {
+                        var compareLetter = function() {
 
-                                for (var i = 0; i <= letterArray.length; i++) {
-                                        if (userletter === letterArray[i]) {
+                                for (var i = 0; i < letterArray.length; i++) {
+                                        if (userLetter === letterArray[i]) {
                                          alert(true);
-                                } else {
+                                        } else {
                                         alert(false);
-                                 }  
+                                        }  
 
-                                }
-                        }              
+                                } //compareLetter "for" loop closeing bracket
+
+                        } //compareLetter closing bracket             
+                        compareLetter();
+
+        } //chosenLetter closing bracket
+        
+        document.removeEventListener("keyup", startGame);
+        document.addEventListener("keyup", chosenLetter);
+        
 
 
-                }
-
-                document.addEventListener("keyup", chosenLetter)
-
-}
+} //startGame Closing Bracket
 
 document.addEventListener("keyup", startGame); 
 
