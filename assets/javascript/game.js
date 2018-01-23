330d73014a9a2d07eed4a86bdbf70a9eb4b16061
Susan Lippa <sfl@Susans-MacBook-Pro.local>
2018-01-22 22:17:39 -0600

    still working JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,6 +29,8 @@ var startGame = function(event) {
                 //console.log(letterArray);// letterArray[0], letterArray[1], letterArray[2], letterArray[3], letterArray[4]);
                 //console.log(typeof letterArray[0]);
 
+
+  
         var letterOne = document.querySelector("#letter1").innerText = letterArray[0];
                 //console.log(letterOne);
         var letterTwo = document.querySelector("#letter2").innerText = letterArray[1];
@@ -38,7 +40,14 @@ var startGame = function(event) {
         var letterFour = document.querySelector("#letter4").innerText = letterArray[3];
                 //console.log(letterFour);
         var letterFive = document.querySelector("#letter5").innerText = letterArray[4];
-                //console.log(letterFive);  
+                //console.log(letterFive); 
+                
+        //var letterColorChange = document.querySelector("#letterColor").classList.add("textColor");
+
+        // for (var j = 0; j < letterArray.length; j++) {
+        //         letterArray[j].document.querySelector("#letterColor").classList.add("textColor");
+        // }
+
         
         directions.innerText = "Choose a letter using the keyboard.";
         directionsBox.style.backgroundColor = "violet";
@@ -58,13 +67,74 @@ var startGame = function(event) {
                         var compareLetter = function() {
 
                                 for (var i = 0; i < letterArray.length; i++) {
-                                        if (userLetter === letterArray[i]) {
-                                         alert(true);
-                                        } else {
-                                        alert(false);
-                                        }  
+                                       if (userLetter === letterArray[i]) {
+                                         userLettersArray[i] = userLetter;
+                                         console.log(userLettersArray);
+                                         
+                                         //letterArray[i] = displayLetter.style.display = "block";
+                                         
+                                          directions.innerText = "The letter " + userLetter + " matches!";  
 
-                                } //compareLetter "for" loop closeing bracket
+                                       } else {}
+                                         
+                                
+
+                                 var compareArrays = function() {
+
+                                        if (chosenWordArray === userLetterArray.join("")) {
+                                                        gamesWon++;
+                                                        livesNumberRender.innerText = gamesWon;
+                                                        directions.innerText = "You win!! Hit 'Enter' if you would like to play again. "
+                                                        directionsBox.style.backgroundColor = "yellow";
+
+                                                } else {
+                                                        directions.innerText = "The letter '" + chosenLetter + "' did not match. Pick another letter.";
+                                
+                                                        guessNum--;
+                                
+                                                        guessNumberRender.innerText = guessNum;
+                                
+                                                        switch(guessNum) {
+                                
+                                                                case 7:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_0.jpeg";
+                                                                        break;
+                                                                case 6:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_1.jpeg";
+                                                                        break
+                                                                case 5:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_2.jpeg";
+                                                                        break;
+                                                                case 4:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_3.jpeg";
+                                                                        break;
+                                                                case 3:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_4.jpeg";
+                                                                        break;
+                                                                case 2:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_5.jpeg";
+                                                                        break;
+                                                                case 1:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_6.jpeg";
+                                                                        break;
+                                                                case 0:
+                                                                        document.querySelector("#image1").src = "file:///Users/sfl/Desktop/WorkingFolder/week02/02-Homework/Hangman-Game/assets/images/Hangman_7.jpeg";     
+                                                                        break;
+                                                                default:
+                                                                alert("Broken Picture");
+                                                
+                                                
+                                                
+                                                
+                                }//compareArray closing bracket
+
+
+                        
+
+
+                     
+
+                                } //compareLetter "for" loop closing bracket
 
                         } //compareLetter closing bracket             
                         compareLetter();
